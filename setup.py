2eed84bf1e8d22ef9baf55d5ae44520855816897
v.selyuh <v.selyuh@smartweb.com.ua>
2021-02-24 18:50:26 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='logevo',
+    version='0.0',
+    description='Facke Logevo',
+    packages=['logevo'],
+    author_email='volodymyr.volodymyrovych@gmail.com',
+    zip_safe=False
+)
